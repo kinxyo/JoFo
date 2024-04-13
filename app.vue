7e5a78e1282f8c9081530d2392d47ae3ba23d9eb
Kinjalk Tripathi <kinjalkt3@gmail.com>
2024-04-13 23:58:23 +0530

    test

--- app.vue
+++ app.vue
@@ -1,9 +1,43 @@
-<script setup lang="ts">
+<script setup>
+
+console.log("sign");
+
+import init, { greets, add } from "~/wasm-pkg/wasmfile.js"
+// import "~/assets/scripts/script.js"
+
+const greeting = ref("");
+const input1 = ref(0);
+const input2 = ref(0);
+const result = ref(0);
+
+onBeforeMount(() => {
+	console.log("working on initializing WASM...");
+	init().then(() => {
+		console.log("WASM initialized!");
+		greeting.value = greets();
+	});	
+	
+});
+
+function using_rust_add(a, b) {
+	console.log("`add` button working");
+	result.value = add(a, b)
+}
+
 </script>
 
+<template>
+	<main>
+		<h1>{{ greeting }}</h1>
+		<div class="loading"></div>
 
-<template><h1> Hey </h1></template>
-
-
-<style>
-</style>
+		<div class="set">
+			<h1 v-if="!result">add 2 numbers</h1>
+			<h1 v-else>{{ result }}</h1>
+			<input type="number" v-model="input1"/>
+			<input type="number" v-model="input2"/>
+			<button @click="using_rust_add(input1, input2)"> ADD </button>
+		</div>
+		
+	</main>
+</template>
