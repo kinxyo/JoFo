0d4bd7d7a31280f921ae91c0e2f35df65f162689
Kinjalk Tripathi <kinjalkt3@gmail.com>
2024-04-14 12:14:33 +0530

    test11

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,13 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+  routeRules: {
+    '/': { prerender: true }
+  },
   nitro: {
     experimental: {
       wasm: true
     }
   },
-  ssr: false,
+  ssr: true,
 })
