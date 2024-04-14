880c5f8da8e57a0a128f4ef2dbb3da15fd1b314c
Kinjalk Tripathi <kinjalkt3@gmail.com>
2024-04-14 16:06:09 +0530

    .

--- composables/wasm.client.js
+++ composables/wasm.client.js
@@ -1,22 +1,18 @@
-import init, { greets, add } from "~/pkg/wasmfile.js"
+import init, { greets, add } from "~/pkg/wasmfile.js";
 
 export default function useWasm() {
-  const greeting = ref("");
-  const input1 = ref(0);
-  const input2 = ref(0);
-  const result = ref(0);
+	const greeting = ref("");
+	const input1 = ref(0);
+	const input2 = ref(0);
+	const result = ref(0);
 
-  onMounted(() => {
-    if (process.client) {        
-        init().then(() => {
-            greeting.value = greets();
-        });  
-    }
-  });
+	init().then(() => {
+		greeting.value = greets();
+	});
 
-  function using_rust_add(a, b) {
-    result.value = add(a, b)
-  }
+	function using_rust_add(a, b) {
+		result.value = add(a, b);
+	}
 
-  return { greeting, input1, input2, result, using_rust_add };
+	return { greeting, input1, input2, result, using_rust_add };
 }
